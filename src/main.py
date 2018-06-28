d1769741a4f792eafa72b094a56b2ea9dddb87fc
Dany Haddad <dhaddad@cognitivescale.com>
2018-06-28 15:26:45 -0500

    fix batch collations

--- src/main.py
+++ src/main.py
@@ -9,8 +9,9 @@ from mention_context_batch_sampler import MentionContextBatchSampler
 import math
 import pickle
 from random import shuffle
+import pydash as _
 
-def load_entity_candidates_lookup(path='../entity-linking-preprocessing/candidate_lookup.pkl'):
+def load_entity_candidates_and_label_lookup(path='../entity-linking-preprocessing/lookups.pkl'):
   with open(path, 'rb') as lookup_file:
     return pickle.load(lookup_file)
 
@@ -19,18 +20,33 @@ def get_page_id_order(cursor):
   page_ids = []
   while True:
     results = cursor.fetchmany(10000)
-    if results is None: break
-    page_ids.append([row['id'] for row in results])
-  return shuffle(page_ids)
+    if _.is_empty(results): break
+    page_ids.extend([row['id'] for row in results])
+  shuffle(page_ids)
+  return page_ids
+
+def get_num_entities(cursor):
+  cursor.execute('select count(*) from entities')
+  return cursor.fetchone()['count(*)']
+
+def collate(batch):
+  max_num_candidates = max(map(len, [sample['candidates'] for sample in batch]))
+  return {'sentence_splits': [sample['sentence_splits'] for sample in batch],
+          'label': torch.tensor([sample['label'] for sample in batch]),
+          'document_mention_indices': [sample['document_mention_indices'] for sample in batch],
+          'candidates': [sample['candidates'] for sample in batch]}
 
 def main():
   try:
-    batch_size = 1000
+    batch_size = 10
     num_epochs = 1000
-    max_num_mentions = 100000
+    max_num_mentions = 10000000
     db_connection = get_connection()
     with db_connection.cursor() as cursor:
-      entity_candidates_lookup = load_entity_candidates_lookup()
+      print('Loading entity candidates lookup')
+      lookups = load_entity_candidates_and_label_lookup()
+      entity_candidates_lookup = lookups['entity_candidates']
+      entity_label_lookup = lookups['entity_labels']
       embed_len = 100
       context_embed_len = 2 * embed_len
       print('Creating word embedding lookup')
@@ -41,10 +57,11 @@ def main():
       dataset = MentionContextDataset(cursor,
                                       page_id_order,
                                       entity_candidates_lookup,
+                                      entity_label_lookup,
                                       batch_size,
                                       max_num_mentions)
       batch_sampler = MentionContextBatchSampler(cursor, page_id_order, batch_size)
-      dataloader = DataLoader(dataset, batch_size=batch_size, batch_sampler=batch_sampler)
+      dataloader = DataLoader(dataset, batch_sampler=batch_sampler, collate_fn=collate)
       num_entities = get_num_entities(cursor)
       embed_len = 100
       entity_embed_weights = nn.Parameter(torch.Tensor(num_entities, embed_len))
@@ -70,5 +87,15 @@ def main():
   finally:
     db_connection.close()
 
+if __name__ == "__main__":
+  import pdb
+  import traceback
+  import sys
 
-if __name__ == "__main__": main()
+  try:
+    main()
+  except:
+    extype, value, tb = sys.exc_info()
+    traceback.print_exc()
+    pdb.post_mortem(tb)
+  # main()
