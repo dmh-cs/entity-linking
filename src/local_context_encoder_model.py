9b12e6f61f5ee8cece8e44171332a92fadf9d810
Dany Haddad <dhaddad@cognitivescale.com>
2018-06-28 15:25:16 -0500

    local context encoder uses last lstm hidden state

--- src/local_context_encoder_model.py
+++ src/local_context_encoder_model.py
@@ -12,19 +12,24 @@ class LocalContextEncoder(nn.Module):
     self.dropout_keep_prob = dropout_keep_prob
     self.left_lstm = nn.LSTM(input_size=self.word_embed_len,
                              hidden_size=self.lstm_size,
+                             num_layers=self.num_lstm_layers,
                              dropout=self.dropout_keep_prob,
                              batch_first=True)
     self.right_lstm = nn.LSTM(input_size=self.word_embed_len,
                               hidden_size=self.lstm_size,
+                              num_layers=self.num_lstm_layers,
                               dropout=self.dropout_keep_prob,
                               batch_first=True)
     self.projection = nn.Linear(2 * self.lstm_size, self.context_embed_len)
     self.relu = nn.ReLU()
 
   def forward(self, sentence_splits):
-    mention_left_sequences = sentence_splits[:, 0]
-    mention_right_sequences = sentence_splits[:, 1]
-    sentence_embed = torch.cat((self.left_lstm(mention_left_sequences),
-                                self.right_lstm(mention_right_sequences)),
+    mention_left_sequences = torch.stack([split[0] for split in sentence_splits])
+    mention_right_sequences = torch.stack([split[1] for split in sentence_splits])
+    left_output, left_state_info = self.left_lstm(mention_left_sequences)
+    right_output, right_state_info = self.right_lstm(mention_right_sequences)
+    left_last_hidden_state = left_state_info[0][-1]
+    right_last_hidden_state = right_state_info[0][-1]
+    sentence_embed = torch.cat((left_last_hidden_state, right_last_hidden_state),
                                dim=1)
     return self.relu(self.projection(sentence_embed))
