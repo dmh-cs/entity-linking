94c0a4e87c3ee11cd420d62ca5a76e8f7fb20b18
Dany Haddad <dhaddad@cognitivescale.com>
2018-07-02 10:14:24 -0500

    get candidate entities

--- src/mention_context_dataset.py
+++ src/mention_context_dataset.py
@@ -2,6 +2,7 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 import pydash as _
+import random
 
 from data_transformers import get_mention_sentence_splits
 from parsers import parse_for_sentences
@@ -14,20 +15,22 @@ class MentionContextDataset(Dataset):
                entity_candidates_lookup,
                entity_label_lookup,
                batch_size,
+               num_entities,
                num_mentions,
                transform=None):
     self.page_id_order = page_id_order
-    self.entity_candidates_lookup = entity_candidates_lookup
+    self.entity_candidates_lookup = _.map_values(entity_candidates_lookup, lambda val: torch.tensor(val))
     self.entity_label_lookup = entity_label_lookup
     self.cursor = cursor
     self.transform = transform
     self.batch_size = batch_size
     self.num_mentions = num_mentions
+    self.num_entities = num_entities
     self._page_id_sentences_lookup = {}
     self._document_mention_lookup = {}
     self._mention_infos = {}
-    self._entity_label_ctr = 0
     self._num_seen_mentions = 0
+    self._num_candidates = 30
     self.page_ctr = 0
 
   def __len__(self):
@@ -40,17 +43,31 @@ class MentionContextDataset(Dataset):
       self._next_batch()
     mention_info = self._mention_infos[idx]
     sentences = self._page_id_sentences_lookup[mention_info['page_id']]
-    if mention_info['entity_id'] not in self.entity_label_lookup:
-      self.entity_label_lookup[mention_info['entity_id']] = self._entity_label_ctr
-      self._entity_label_ctr += 1
+    label = self.entity_label_lookup[mention_info['entity_id']]
     sample = {'sentence_splits': get_mention_sentence_splits(sentences, mention_info),
-              'label': self.entity_label_lookup[mention_info['entity_id']],
+              'label': label,
               'document_mention_indices': self._document_mention_lookup[mention_info['page_id']],
-              'candidates': self.entity_candidates_lookup[mention_info['mention_id']]}
+              'candidates': self._get_candidates(mention_info['mention'], label)}
     if self.transform:
       sample = self.transform(sample)
     self._num_seen_mentions += 1
     return sample
+
+  def _get_candidates(self, mention, label):
+    base_candidates = self.entity_candidates_lookup[mention]
+    if len(base_candidates) < self._num_candidates:
+      indexes_to_keep = range(len(base_candidates))
+    else:
+      label_index = int((base_candidates == label).nonzero().squeeze())
+      indexes_to_sample = set(range(len(base_candidates))) - set([label_index])
+      indexes_to_keep = random.sample(indexes_to_sample,
+                                      self._num_candidates - 1) + [label_index]
+    num_candidates_to_generate = self._num_candidates - len(indexes_to_keep)
+    if num_candidates_to_generate != 0:
+      random_candidates = torch.tensor(random.sample(range(self.num_entities), num_candidates_to_generate))
+      return torch.cat((base_candidates[indexes_to_keep], random_candidates), 0)
+    else:
+      return base_candidates[indexes_to_keep]
 
   def _get_mention_infos_by_page_id(self, page_id):
     self.cursor.execute('select mention, page_id, entity_id, mention_id, offset from entity_mentions_text where page_id = %s', page_id)
