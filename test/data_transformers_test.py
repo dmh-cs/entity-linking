707641adf9eeb491538582167dfe11413b746df7
Dany Haddad <dhaddad@cognitivescale.com>
2018-07-09 15:23:54 -0500

    remove old test

--- test/data_transformers_test.py
+++ test/data_transformers_test.py
@@ -2,27 +2,6 @@ import data_transformers as dt
 import torch
 import pydash as _
 
-def test_raw_datasets_to_desc_encoder_inputs():
-  entity_lookup = {'a': 1, 'b':2}
-  embedding_lookup = {'verb': torch.tensor([1, 2, 3]),
-                      'word': torch.tensor([2, 3, 4]),
-                      '<PAD>': torch.tensor([0, 0, 0]),
-                      '<UNK>': torch.tensor([1, 1, 1])}
-  raw_datasets = {'train': [{'title': 'a', 'content': 'verb verb word verb'}]}
-  datasets = dt.raw_datasets_to_desc_encoder_inputs(entity_lookup, embedding_lookup, raw_datasets)
-  result = {'train': [(torch.tensor([[1, 2, 3],
-                                     [1, 2, 3],
-                                     [2, 3, 4],
-                                     [1, 2, 3]] + [[0, 0, 0]] * 96),),
-                      (1,)]}
-  ctr = 0
-  descriptions, label = datasets['train']
-  for description, label in zip(descriptions, label):
-    assert torch.equal(description, result['train'][0][0])
-    assert label == result['train'][1][0]
-    ctr += 1
-  assert ctr == 1
-
 def test_get_mention_sentence_splits():
   page_content = 'a b c aa bb cc'
   sentence_spans = [(0, 5), (6, 14)]
