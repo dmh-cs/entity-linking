27e505c528c94942e43abd679e982e82c4557008
Dany Haddad <dhaddad@cognitivescale.com>
2018-07-17 08:11:30 -0500

    single directional lstm for document encoder

--- src/document_context_encoder_model.py
+++ src/document_context_encoder_model.py
@@ -9,7 +9,6 @@ from data_transformers import pad_batch
 
 class DocumentContextEncoder(nn.Module):
   def __init__(self,
-               dropout_keep_prob,
                lstm_size,
                word_embed_len,
                context_embed_len,
@@ -18,13 +17,11 @@ class DocumentContextEncoder(nn.Module):
     self.lstm_size = lstm_size
     self.word_embed_len = word_embed_len
     self.context_embed_len = context_embed_len
-    self.dropout_keep_prob = dropout_keep_prob
     self.pad_vector = pad_vector
     self.lstm = nn.LSTM(input_size=self.word_embed_len,
                         hidden_size=self.lstm_size,
-                        bidirectional=True,
                         batch_first=True)
-    self.projection = nn.Linear(2 * self.lstm_size, self.context_embed_len)
+    self.projection = nn.Linear(self.lstm_size, self.context_embed_len)
     self.relu = nn.ReLU()
 
   def forward(self, embedded_page_contents):
