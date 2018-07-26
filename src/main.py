60ad84efea2b2bf586536accda08a2ce28d92654
Dany Haddad <dhaddad@cognitivescale.com>
2018-07-26 08:19:58 -0500

    restore run params

--- src/main.py
+++ src/main.py
@@ -15,13 +15,15 @@ def main():
   flags = [_.head(arg) for arg in getopt.getopt(_.tail(sys.argv), '', ['load_model'])[0]]
   load_model = '--load_model' in flags
   train_params = m(load_model=load_model)
+  run_params = m(load_model=load_model)
   model_params = m()
   paths = m(lookups=os.getenv("LOOKUPS_PATH"),
             page_id_order=os.getenv("PAGE_ID_ORDER_PATH"))
   runner = Runner(device=device,
                   paths=paths,
                   train_params=train_params,
-                  model_params=model_params)
+                  model_params=model_params,
+                  run_params=run_params)
   runner.run()
 
 
