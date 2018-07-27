748dc934805dcd9c35dc3d2c9df3273c2ca6327b
Dany Haddad <dhaddad@cognitivescale.com>
2018-07-27 17:43:59 -0500

    parse command line arguments

--- src/main.py
+++ src/main.py
@@ -9,20 +9,41 @@ import torch
 
 from runner import Runner
 
+args_with_values = [{'name': 'model_path', 'for': 'path', 'type': str},
+                    {'name': 'batch_size', 'for': 'train_param', 'type': int},
+                    {'name': 'num_epochs', 'for': 'train_param', 'type': int},
+                    {'name': 'train_size', 'for': 'train_param', 'type': int},
+                    {'name': 'dropout_keep_prob', 'for': 'train_param', 'type': float},
+                    {'name': 'embed_len', 'for': 'model_param', 'type': int},
+                    {'name': 'word_embed_len', 'for': 'model_param', 'type': int},
+                    {'name': 'num_candidates', 'for': 'model_param', 'type': int},
+                    {'name': 'local_encoder_lstm_size', 'for': 'model_param', 'type': int},
+                    {'name': 'document_encoder_lstm_size', 'for': 'model_param', 'type': int},
+                    {'name': 'num_lstm_layers', 'for': 'model_param', 'type': int},
+                    {'name': 'word_embedding_set', 'for': 'model_param', 'type': str}]
+
 def main():
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
   load_dotenv(dotenv_path='.env')
-  args = getopt.getopt(_.tail(sys.argv), '', ['load_model', 'model_path=', 'name='])[0]
+  args = getopt.getopt(_.tail(sys.argv), '', ['load_model'] + [arg['name'] + '=' for arg in args_with_values])[0]
   flags = [_.head(arg) for arg in args]
-  model_path_pair = _.find(args, lambda pair: 'model_path' in pair[0])
-  name_pair = _.find(args, lambda pair: 'name' in pair[0])
   train_params = m()
   run_params = m(load_model='--load_model' in flags)
-  model_params = m()
+  model_params = m(ablation=['local_context'])
   paths = m(lookups=os.getenv("LOOKUPS_PATH"),
             page_id_order=os.getenv("PAGE_ID_ORDER_PATH"))
-  if model_path_pair:
-    paths = paths.set('model', model_path_pair[1])
+  for arg in args_with_values:
+    name = arg['name']
+    pair = _.find(args, lambda pair: name in pair[0])
+    if pair:
+      parsed = arg['type'](pair[1])
+      if arg['for'] == 'path':
+        paths = paths.set(name, parsed)
+      elif arg['for'] == 'model_param':
+        model_params = model_params.set(name, parsed)
+      elif arg['for'] == 'train_param':
+        train_params = train_params.set(name, parsed)
+  name_pair = _.find(args, lambda pair: 'name' in pair[0])
   name = name_pair[1] if name_pair else ''
   runner = Runner(device=device,
                   paths=paths,
