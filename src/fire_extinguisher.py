350281c6899211c6baae159ddb1634906a3302a8
Dany Haddad <dhaddad@cognitivescale.com>
2018-09-14 09:41:26 -0500

    add pytorch debugging tools (fire extinguisher)

---
+++ src/fire_extinguisher.py
@@ -0,0 +1,18 @@
+class BatchRepeater():
+  def __init__(self, sampler, num_repetitions=1, repeat_first=True):
+    self.batch_to_repeat = None
+    self.sampler = iter(sampler)
+    self.num_repetitions = num_repetitions
+    self.repetition_ctr = 0
+    if not repeat_first:
+      raise NotImplementedError
+
+  def __len__(self):
+    return self.num_repetitions
+
+  def __iter__(self):
+    if self.batch_to_repeat is None:
+      self.batch_to_repeat = next(self.sampler)
+    while self.repetition_ctr < self.num_repetitions:
+      self.repetition_ctr += 1
+      yield self.batch_to_repeat
