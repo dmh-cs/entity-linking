71415078bbeac2b9c7213fb64c09b647cf24506f
Dany Haddad <dhaddad@cognitivescale.com>
2018-10-12 09:38:13 -0500

    make sure conv is correct

--- src/description_encoder_model.py
+++ src/description_encoder_model.py
@@ -23,10 +23,11 @@ class DescriptionEncoder(nn.Module):
                             [embeds[:100] for embeds in embedded_page_contents],
                             min_len=100)
     encoded = pipe(desc_embeds,
-                   # lambda embed: torch.transpose(embed, 1, 2),
+                   lambda embed: torch.transpose(embed, 1, 2),
                    self.conv,
                    self.relu,
                    self.dropout,
                    self.global_avg_pooling,
+                   lambda embed: torch.transpose(embed, 1, 2),
                    torch.squeeze)
     return encoded / torch.norm(encoded, 2, 1).unsqueeze(1)
