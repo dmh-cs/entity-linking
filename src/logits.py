3f272bad38d8ef2d8246145121c95860ebf3b797
Dany Haddad <dhaddad@cognitivescale.com>
2018-10-22 08:55:38 -0500

    logits should not be module

--- src/logits.py
+++ src/logits.py
@@ -1,11 +1,7 @@
 import torch
-from torch import nn
 
-class Logits(nn.Module):
-  def __init__(self):
-    super().__init__()
-
-  def forward(self, hidden, candidates: torch.Tensor):
+class Logits():
+  def __call__(self, hidden, candidates: torch.Tensor):
     return torch.sum(torch.mul(torch.unsqueeze(hidden, 1),
                                candidates),
                      2)
