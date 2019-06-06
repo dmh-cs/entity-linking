c62c037c91ced56244fc6eec4709fba8811b957d
Dany Haddad <dhaddad@cognitivescale.com>
2019-06-06 15:12:34 -0500

    use cache for simple conll

--- src/simple_conll_dataset.py
+++ src/simple_conll_dataset.py
@@ -13,12 +13,13 @@ from parsers import parse_text_for_tokens
 import utils as u
 from data_transformers import get_mention_sentences_from_infos, pad_batch_list
 
-
-def get_desc_fs(cursor, stemmer, cand_ids):
+def get_desc_fs(desc_fs, cursor, stemmer, cand_ids):
   def _process(page_content):
     tokenized = parse_text_for_tokens(page_content)
     return dict(Counter(stemmer.stem(token) for token in tokenized))
-  cursor.execute('select ep.entity_id as entity_id, left(p.content, 2000) as content from entity_by_page ep join pages p on ep.source_id = p.source_id where ep.entity_id in (' + str(cand_ids)[1:-1] + ')')
+  cached_fs = {cand_id: desc_fs[cand_id] for cand_id in cand_ids if cand_id in desc_fs}
+  remaining_cand_ids = [cand_id for cand_id in cand_ids if cand_id not in cached_fs]
+  cursor.execute('select ep.entity_id as entity_id, left(p.content, 2000) as content from entity_by_page ep join pages p on ep.source_id = p.source_id where ep.entity_id in (' + str(remaining_cand_ids)[1:-1] + ')')
   return {row['entity_id']: _process(row['content']) for row in cursor.fetchall()}
 
 def clean_entity_text(entity_text):
@@ -61,6 +62,7 @@ class SimpleCoNLLDataset(Dataset):
         self.with_labels.append(idx)
       else:
         num_without_cand += 1
+    self.desc_fs = {}
     print('num without candidates:', num_without_cand)
     print('num with candidates:', len(self.with_labels))
 
@@ -90,7 +92,7 @@ class SimpleCoNLLDataset(Dataset):
     candidate_mention_sim = [Levenshtein.ratio(mention, clean_entity_text(candidate_str))
                              for candidate_str in candidate_strs]
     all_mentions_features = []
-    candidate_fs = get_desc_fs(self.cursor, self.stemmer, candidate_ids)
+    candidate_fs = get_desc_fs(self.desc_fs, self.cursor, self.stemmer, candidate_ids)
     cands_with_page = []
     for candidate_raw_features in zip(candidate_ids,
                                       candidate_mention_sim,
